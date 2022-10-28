bfcbd0d64f386e686a8f6452a142e1da494799f3
hide7789 <hide7789@nate.com>
2022-10-28 18:59:09 +0900

    Create Harshad.java

---
+++ Harshad.java
@@ -0,0 +1,19 @@
+class Solution {
+	public boolean solution(int x) {
+		boolean answer = true;
+		int sum = 0;
+		int n = x;
+
+		while (x > 0) {
+			sum += x % 10;
+			x /= 10;
+
+		}
+		if (n % sum == 0) {
+			return answer;
+		} else {
+			return false;
+		}
+
+	}
+}
